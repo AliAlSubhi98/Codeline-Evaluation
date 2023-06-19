21173641f41cb53a6c2b1a9dc2eb6b5ab0e7a53f
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-06-19 10:59:37 +0400

    create UserRegistrationRequest

---
+++ src/main/java/com/alialsubhi/Codeline/Evaluation/Requests/UserRegistrationRequest.java
@@ -0,0 +1,13 @@
+package com.alialsubhi.Codeline.Evaluation.Requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationRequest {
+
+    private String username;
+    private String password;
+
+}
