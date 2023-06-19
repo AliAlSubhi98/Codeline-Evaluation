629000431a55c61d3ce8e77aca0224a072ae426b
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-06-19 15:28:29 +0400

    working with EventService { method to search for an Event Using @Query, The return type of the search method is a List<Event>, representing the list of events that match the specified criteria. }

--- src/main/java/com/alialsubhi/Codeline/Evaluation/Repo/EventRepo.java
+++ src/main/java/com/alialsubhi/Codeline/Evaluation/Repo/EventRepo.java
@@ -2,8 +2,23 @@ package com.alialsubhi.Codeline.Evaluation.Repo;
 
 import com.alialsubhi.Codeline.Evaluation.Models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface EventRepo extends JpaRepository <Event,Long> {
+
+    @Query("SELECT e FROM Event e WHERE (:location IS NULL OR e.location = :location)"
+            + " AND (:dateFrom IS NULL OR e.date >= :dateFrom)"
+            + " AND (:dateTo IS NULL OR e.date <= :dateTo)"
+            + " AND (:eventName IS NULL OR e.name LIKE %:eventName%)")
+    List<Event> search(@Param("location") String location,
+                       @Param("dateFrom") LocalDate dateFrom,
+                       @Param("dateTo") LocalDate dateTo,
+                       @Param("eventName") String eventName);
+
 }
--- src/main/java/com/alialsubhi/Codeline/Evaluation/Services/EventService.java
+++ src/main/java/com/alialsubhi/Codeline/Evaluation/Services/EventService.java
@@ -5,6 +5,11 @@ import com.alialsubhi.Codeline.Evaluation.Repo.EventRepo;
 import com.alialsubhi.Codeline.Evaluation.Requests.EventCreationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class EventService {
@@ -25,5 +30,11 @@ public class EventService {
     public Event getEventByID(Long id){
         return eventRepository.findById(id).get();
     }
+
+    public List<Event> searchEvents(String location, LocalDate dateFrom, LocalDate dateTo, String eventName) {
+        return eventRepository.search(location, dateFrom, dateTo, eventName);
+    }
+
+    
 
 }
