3debc23b04d1e725997efaf163e1bc98d7bfc8a8
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-06-19 10:25:38 +0400

    init commit

---
+++ src/main/java/com/alialsubhi/Codeline/Evaluation/CodelineEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.alialsubhi.Codeline.Evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelineEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelineEvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alialsubhi/Codeline/Evaluation/CodelineEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alialsubhi.Codeline.Evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelineEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
